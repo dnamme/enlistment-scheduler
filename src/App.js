9fd7724b280dd4e6032622d7b203c88971359f03
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-08-18 00:44:33 +0800

    add timetable data and basic css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import InputContainer from './components/InputContainer'
 import Timetable from './components/Timetable'
 import './css/App.css'
@@ -5,10 +7,17 @@ import './css/Input.css'
 import './css/Timetable.css'
 
 function App() {
+  const [data, setData] = useState([])
+  const [groupedData, setGroupedData] = useState([[], [], [], [], [], []])
+
+  useEffect(() => {
+    console.log('data changed!')
+  }, data)
+
   return (
     <div className="app">
       <InputContainer />
-      <Timetable />
+      <Timetable data={groupedData} />
     </div>
   )
 }
--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -1,6 +1,13 @@
-function Timetable() {
+function Timetable({ data }) {
+  const daysOfWeek = ['M', 'T', 'W', 'Th', 'F', 'S']
+
   return (
     <div className="timetable">
+      {data.map((col, index) => (
+        <div key={index}>
+          <p className="day">{daysOfWeek[index]}</p>
+        </div>
+      ))}
     </div>
   )
 }
