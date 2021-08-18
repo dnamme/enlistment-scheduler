b0e70da998f52cf9981dbbba317c2ec9ee5bbb57
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-08-18 13:15:09 +0800

    add form state, add modal border radius

--- src/components/CopyModal.js
+++ src/components/CopyModal.js
@@ -1,12 +1,26 @@
+import { useState } from 'react'
+
 function CopyModal({ onExit, onCopySubmit }) {
+  const [data, setData] = useState('')
+
   return (
     <div className="modal">
       {/* <div className="modal-backdrop" onClick={onExit}></div> */}
       <div className="modal-content">
         <h3>Go to Class Schedule on AISIS and paste rows from the tables below</h3>
         <form className="copy-form">
-          <input type="text" placeholder="Paste here..."></input>
-          <input class="button" type="submit" value="Add" onClick={onCopySubmit}></input>
+          <input
+            type="text"
+            placeholder="Paste here..."
+            value={data}
+            onChange={(e) => setData(e.target.value)}
+          />
+          <input
+            type="submit"
+            class="button"
+            value="Add"
+            onClick={onCopySubmit}
+          />
         </form>
       </div>
     </div>
