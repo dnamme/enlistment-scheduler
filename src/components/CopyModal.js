ca389305506b18221ba732a087a1be79a309f4b2
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2021-08-21 23:34:38 +0800

    added style and text in copy modal

--- src/components/CopyModal.js
+++ src/components/CopyModal.js
@@ -8,7 +8,18 @@ function CopyModal({ keyCode, onAddClick, onExitClick }) {
   return (
     <div className="modal">
       <div className="modal-content">
-        <h4>Go to Class Schedule on AISIS and paste rows from the tables below</h4>
+        <h5 style={{ margin: 0, alignSelf: 'flex-start' }}>How to add classes from AISIS:</h5>
+
+        <ol style={{ paddingLeft: '24px', alignSelf: 'flex-start' }}>
+          <li>Go to AISIS Online and navigate to Class Schedule</li>
+          <li>Click the Display Class Schedule button for any department, course, or category number</li>
+          <li>Copy the entire row (or entire rows for multiple courses) from the table on AISIS
+            <ul>
+              <li>Don't worry if there are extra spaces! The tool will work as long as you don't type or change anything important!</li>
+            </ul>
+          </li>
+          <li>Paste the text in the textbox below</li>
+        </ol>
 
         <textarea
           rows="5"
@@ -18,7 +29,7 @@ function CopyModal({ keyCode, onAddClick, onExitClick }) {
           onChange={(e) => setData(e.target.value)}
         />
 
-        <FlatButton cStyle={{ width: '128px' }} text="Add" onClick={() => onAddClick(keyCode, data)} />
+        <FlatButton cStyle={{ width: '128px', margin: '32px 0 16px 0' }} text="Add" onClick={() => onAddClick(keyCode, data)} />
         <FlatButton cStyle={{ width: '128px' }} text="Cancel" isOutlined={true} onClick={onExitClick} />
       </div>
     </div>
